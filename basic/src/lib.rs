b77c83b86c5cde451f6ef54910714ecf1a17e36c
Max Gillett <max.gillett@gmail.com>
2023-05-21 13:54:24 -0400

    chore: remove comments in basic crate

--- basic/src/lib.rs
+++ basic/src/lib.rs
@@ -4,10 +4,7 @@ extern crate alloc;
 
 use valida_alu_u32::{
     add::{Add32Chip, Add32Instruction, MachineWithAdd32Chip},
-    //div::{Div32Instruction, MachineWithDiv32Chip},
-    //lt::{Lt32Instruction, MachineWithLt32Chip},
     mul::{MachineWithMul32Chip, Mul32Chip, Mul32Instruction},
-    //sub::{MachineWithSub32Chip, Sub32Chip, Sub32Instruction},
 };
 use valida_bus::{CpuMemBus, MachineWithGeneralBus, MachineWithMemBus, SharedCoprocessorBus};
 use valida_cpu::{
@@ -42,14 +39,9 @@ pub struct BasicMachine {
     // ALU instructions
     #[instruction(add_u32)]
     add32: Add32Instruction,
-    //#[instruction(sub_u32)]
-    //sub32: Sub32Instruction,
     #[instruction(mul_u32)]
     mul32: Mul32Instruction,
-    //#[instruction(div_u32)]
-    //div32: Div32Instruction,
-    //#[instruction(lt_u32)]
-    //lt32: LtInstruction,
+
     #[chip]
     cpu: CpuChip<Fp>,
     #[chip]
@@ -58,12 +50,6 @@ pub struct BasicMachine {
     add_u32: Add32Chip<Fp>,
     #[chip]
     mul_u32: Mul32Chip<Fp>,
-    //#[chip]
-    //sub_u32: Sub32Chip<F>,
-    //#[chip]
-    //div_u32: Div32Chip<F>,
-    //#[chip]
-    //lt_u32: Lt32Chip<F>,
 }
 
 impl MachineWithGeneralBus for BasicMachine {
@@ -108,16 +94,6 @@ impl MachineWithAdd32Chip for BasicMachine {
     }
 }
 
-//impl MachineWithSub32Chip for BasicMachine {
-//    fn sub_u32(&self) -> &Sub32Chip {
-//        &self.add_u32
-//    }
-//
-//    fn sub_u32_mut(&mut self) -> &mut Sub32Chip {
-//        &mut self.add_u32
-//    }
-//}
-
 impl MachineWithMul32Chip for BasicMachine {
     fn mul_u32(&self) -> &Mul32Chip<Fp> {
         &self.mul_u32
@@ -127,26 +103,6 @@ impl MachineWithMul32Chip for BasicMachine {
         &mut self.mul_u32
     }
 }
-
-//impl MachineWithDiv32Chip for BasicMachine {
-//    fn div_u32(&self) -> &Div32Chip {
-//        &self.div_u32
-//    }
-//
-//    fn div_u32_mut(&mut self) -> &mut Div32Chip {
-//        &mut self.div_u32
-//    }
-//}
-
-//impl MachineWithLt32Chip for BasicMachine {
-//    fn lt_u32(&self) -> &Lt32Chip {
-//        &self.lt_u32
-//    }
-//
-//    fn lt_u32_mut(&mut self) -> &mut Lt32Chip {
-//        &mut self.lt_u32
-//    }
-//}
 
 #[cfg(test)]
 mod tests {
