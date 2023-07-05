399ec7f168245d36884ee5d644c58a3cb28b6965
Daniel Lubarov <daniel@lubarov.com>
2023-07-05 14:26:38 -0700

    Restructure the prover code a bit
    - Commit to traces for all chips together, using `commit_batches`, so that it can use the more powerful batching Plonky3 supports
    - Change `check_constraints` to read committed data vs the original trace, to avoid cloning (still need to update `check_constraints` also)
    - Remove unused `prover` crate stub

--- machine/src/__internal/mod.rs
+++ machine/src/__internal/mod.rs
@@ -1,3 +1,5 @@
+//! Items intended to be used only by `valida-derive`.
+
 use p3_mersenne_31::Mersenne31;
 
 pub type DefaultField = Mersenne31;
@@ -12,3 +14,8 @@ pub use check_constraints::*;
 pub use debug_builder::*;
 pub use folding_builder::*;
 pub use prove::*;
+
+// Re-export some Plonky3 crates so that derives can use them.
+pub use p3_challenger;
+pub use p3_commit;
+pub use p3_matrix;
--- machine/src/__internal/prove.rs
+++ machine/src/__internal/prove.rs
@@ -1,22 +1,15 @@
 use crate::__internal::ConstraintFolder;
 use crate::config::StarkConfig;
+use crate::proof::ChipProof;
 use crate::{Chip, Machine};
 use p3_air::Air;
-use p3_challenger::Challenger;
-use p3_commit::PCS;
-use p3_matrix::dense::RowMajorMatrix;
 
-pub fn prove<M, A, SC>(
-    machine: &M,
-    config: &SC,
-    air: &A,
-    _challenger: &mut SC::Chal,
-    main: RowMajorMatrix<M::F>,
-    perm: RowMajorMatrix<M::EF>,
-) where
+pub fn prove<M, A, SC>(machine: &M, config: &SC, air: &A, challenger: &mut SC::Chal) -> ChipProof
+where
     M: Machine,
-    SC: StarkConfig<Val = M::F, Challenge = M::EF>,
     A: for<'a> Air<ConstraintFolder<'a, M::F, M::EF, M>> + Chip<M>,
+    SC: StarkConfig<Val = M::F, Challenge = M::EF>,
 {
-    let (_trace_commit, _trace_data) = config.pcs().commit_batch(main);
+    // TODO: Sumcheck
+    ChipProof
 }
