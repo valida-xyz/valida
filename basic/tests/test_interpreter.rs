75e42cc7104c43bc27f3a299b25d07a59ddaf6d9
Max Gillett <max.gillett@gmail.com>
2023-08-13 10:41:17 -0700

    fix: interpreter output

--- basic/tests/test_interpreter.rs
+++ basic/tests/test_interpreter.rs
@@ -45,12 +45,12 @@ fn run_fibonacci() {
 }
 
 fn fibonacci(n: u32) -> u32 {
-    let mut a = 0;
-    let mut b = 1;
+    let mut a = 0u32;
+    let mut b = 1u32;
     for _ in 0..n {
         let temp = a;
         a = b;
-        b = temp + b;
+        (b, _) = temp.overflowing_add(b);
     }
     a
 }
