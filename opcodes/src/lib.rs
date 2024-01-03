c4de06c34a6790a5431ae1458bdcc96e07e83791
thealmarty <thealmartyblog@gmail.com>
2024-01-03 13:22:03 -0800

    Fix opcode.

--- opcodes/src/lib.rs
+++ opcodes/src/lib.rs
@@ -25,9 +25,10 @@ pub const SHR32: u32 = 106;
 pub const AND32: u32 = 107;
 pub const OR32: u32 = 108;
 pub const XOR32: u32 = 109;
-pub const MULHU32: u32 = 112;
-pub const SRA32: u32 = 113;
-pub const MULHS32: u32 = 114;
+pub const NE32: u32 = 111;
+pub const MULHU32 : u32 = 112; //TODO
+pub const SRA32 : u32 = 113; //TODO
+pub const MULHS32 : u32 =114; //TODO
 
 /// NATIVE FIELD
 pub const ADD: u32 = 200;
