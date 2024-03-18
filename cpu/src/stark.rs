24538815cc366dd1a9d576b9ef5b2841f2f2bf53
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2024-03-18 14:21:28 -0400

    issue/122: add missing constraints to zero tests: refactor

--- cpu/src/stark.rs
+++ cpu/src/stark.rs
@@ -267,10 +267,8 @@ impl CpuChip {
         );
         builder.assert_bool(local.not_equal);
         builder.assert_eq(local.not_equal, local.diff * local.diff_inv);
-        builder.assert_eq(
-            AB::Expr::zero(),
-            (AB::Expr::one() - local.not_equal) * local.diff,
-        );
+        let equal = AB::Expr::one() - local.not_equal;
+        builder.assert_zero(equal * local.diff);
     }
 }
 
