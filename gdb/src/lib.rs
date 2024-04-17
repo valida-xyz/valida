73822fad39dce9e499124fdbccb1f6ec385dea5e
Alan Li <alan.li@me.com>
2024-04-17 19:53:21 -0400

    slowly implement the skeletons

--- gdb/src/lib.rs
+++ gdb/src/lib.rs
@@ -1,7 +1,57 @@
-#![no_std]
+#[cfg(unix)]
+use std::os::unix::net::UnixListener;
+#[cfg(unix)]
+use std::os::unix::net::UnixStream;
+
+use std::net::TcpStream;
+use std::net::TcpListener;
+
+use gdbstub::common::Signal;
+use gdbstub::conn::Connection;
+use gdbstub::conn::ConnectionExt;
+use gdbstub::stub::run_blocking;
+use gdbstub::stub::DisconnectReason;
+use gdbstub::stub::GdbStub;
+use gdbstub::stub::SingleThreadStopReason;
+use gdbstub::target::Target;
 
 pub struct Program {
     pub code: i32,
     pub data: u32,
 }
+
+fn wait_for_tcp(port: u16) -> std::io::Result<TcpStream> {
+    let sockaddr = format!("127.0.0.1:{}", port);
+    eprintln!("Waiting for a GDB connection on {:?}...", sockaddr);
+    let sock = TcpListener::bind(sockaddr)?;
+    let (stream, addr) = sock.accept()?;
+    eprintln!("Debugger connected from {}", addr);
+    Ok(stream)
+}
+
+#[cfg(unix)]
+fn wait_for_uds(path: &str) -> std::io::Result<UnixStream> {
+    match std::fs::remove_file(path) {
+        Ok(_) => {}
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => {}
+            _ => return Err(e.into()),
+        },
+    }
+
+    eprintln!("Waiting for a GDB connection on {}...", path);
+
+    let sock = UnixListener::bind(path)?;
+    let (stream, addr) = sock.accept()?;
+    eprintln!("Debugger connected from {:?}", addr);
+
+    Ok(stream)
+}
+
+enum EmuGdbEventLoop {}
+
+// TODO: follow https://github.com/daniel5151/gdbstub/blob/master/examples/armv4t/main.rs
+
+
+
 
