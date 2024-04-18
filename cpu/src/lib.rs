c4f44ac55a8cdf003176ea60baea84a67cc262e4
thealmarty <thealmartyblog@gmail.com>
2024-04-18 13:20:07 -0700

    f

--- cpu/src/lib.rs
+++ cpu/src/lib.rs
@@ -643,7 +643,7 @@ where
 
         state
             .mem_mut()
-            .write(clk, 0, Word::from(0), true);
+            .write(clk, write_addr_index, cell_to_write, true);
         state.cpu_mut().pc += 1;
         state.cpu_mut().push_op(Operation::StoreU8, opcode, ops);
     }
